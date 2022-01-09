6ac42a6e966815748907a41edcf6f2e13721b43a
Omar Awad <o.ashraaf@yahoo.com>
2022-01-09 14:36:20 +0100

    modifing login/register to have sessions

--- myApp/app.js
+++ myApp/app.js
@@ -1,144 +1,186 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
+var session = require('express-session');
 var fs = require('fs');
 var app = express();
-const port = 3000;
+var TWO_HOURS = 1000 * 60 * 60 * 2
+var {
+    PORT = 3000,
+    NODE_ENV = 'development',
+    SESS_NAME = 'sid',
+    SESS_SECRET = 'gjd;jgaklf;jhg',
+    SESS_LIFETIME = TWO_HOURS
+} = process.env
+var IN_PROD = NODE_ENV === 'production'
+// var users = [
+//     { id: 1, username: '3omarashraaf', password:  '12345678',books: []},
+//     { id: 1, username: 'ibraaa33', password:  '12345678',books: []}
+// ]
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  name: SESS_NAME,
+  resave: false,
+  saveUninitialized: false,
+  secret: SESS_SECRET,
+  cookie: {
+      maxAge: SESS_LIFETIME,
+      sameSite: true,
+      secure: IN_PROD
+  }
+}))
+
+const redirectLogin = (req, res, next) =>{
+  if (!req.session.userId){
+      res.redirect('/login')
+  }else{
+      next()
+  }
+}
+const redirectHome = (req, res, next) =>{
+  if (req.session.userId){
+      res.redirect('/home')
+  }else{
+      next()
+  }
+}
+app.use((req, res, next) => {
+  const{userId} = req.session
+  fs.readFile('users.json', 'utf-8', function(err, data) { 
+    if (err) throw err;
+    var existingUsers = JSON.parse(data);
+    var users = existingUsers
+    if (userId) {
+      res.locals.user = users.find(
+          user => user.id === userId
+      )
+    }
+  })
+  next()
+})
+
 
 
 // GET Requests 
-app.get('/',function(req,res){
+app.get('/', (req, res) => {
+  const {userId} = req.session
+  res.send(`
+    <h1>WELCOME!</h1>
+    ${userId ? `
+    <a href = "home">Home</a>
+    <form method= 'post' action = '/logout'>
+        <button>Logout</button>
+    </form>
+    ` : `
+  <a href = "login">Login</a>
+  <a href = "registration">Register</a>
+  `}
+  `)
+})
+app.get('/login', redirectHome,(req,res) => {
   res.render('login');
 });
-
-app.get('/registration',function(req,res){
+app.get('/registration', redirectHome,(req,res) => {
   res.render('registration');
 });
-app.get('/home',function(req,res){
+app.get('/home', redirectLogin,(req,res) => {
+  const {userId} = req.session
+  console.log(userId);
   res.render('home');
 });
-app.get('/novel',function(req,res){
+app.get('/novel', redirectLogin,(req,res) => {
   res.render('novel');
 });
-app.get('/flies',function(req,res){
+app.get('/flies', redirectLogin,(req,res) => {
   res.render('flies');
 });
-app.get('/grapes',function(req,res){
+app.get('/grapes', redirectLogin,(req,res) => {
   res.render('grapes');
 });
-app.get('/poetry',function(req,res){
+app.get('/poetry', redirectLogin,(req,res) => {
   res.render('poetry');
 });
-app.get('/leaves',function(req,res){
+app.get('/leaves', redirectLogin,(req,res) => {
   res.render('leaves');
 });
-app.get('/sun',function(req,res){
+app.get('/sun', redirectLogin,(req,res) => {
   res.render('sun');
 });
-app.get('/fiction',function(req,res){
+app.get('/fiction', redirectLogin,(req,res) => {
   res.render('fiction');
 });
-app.get('/dune',function(req,res){
+app.get('/dune', redirectLogin,(req,res) => {
   res.render('dune');
 });
-app.get('/mockingbird',function(req,res){
+app.get('/mockingbird', redirectLogin,(req,res) => {
   res.render('mockingbird');
 });
-app.get('/readlist',function(req,res){
+app.get('/readlist', redirectLogin,(req,res) => {
   res.render('readlist');
 });
 
 // POST Requests 
-app.post('/register', function(req,res){
-  var userObj = req.body;
-  userObj.books= [];
-  //  Checking on empty entries
-  if(!userObj.username || !userObj.password){
-      console.log("Username/Password cannot be empty!");
-      res.render('registration');
-  }  
-  else{
-      //  Reading from the JSON DB
-      fs.readFile('users.json', 'utf-8', function(err, data) { 
-          if (err) throw err;
-          var existingUsers = JSON.parse(data);
-          //  Check if the username already exists
-          var flag = false; 
-          for (let users of existingUsers.users) {
-            if(users.username == userObj.username){
-                console.log('Username already exists');
-                res.render('registration');
-                flag = true;
-                break;
-            }
+
+app.post('/register', redirectHome, (req,res)=>{
+  const {username,password} = req.body
+  if (username&&password){
+    fs.readFile('users.json', 'utf-8', function(err, data) { 
+      if (err) throw err;
+      var existingUsers = JSON.parse(data);
+      var users = existingUsers
+      const exists = users.some(
+        user => user.username === username
+      )
+      if (!exists){
+        const user2 = {
+          id: users.length + 1,
+          username,
+          password,
+          books: []
         }
-      //  Writing data back to the DB  
-          if(!flag){
-              existingUsers.users.push(userObj);  
-              fs.writeFile('users.json', JSON.stringify(existingUsers), 'utf-8', function(err) {
-                  if (err) throw err
-                  console.log('Done!');
-
-                  return res.redirect('/home');
-              });
-          }     
-      });
-  }
-});
-
-app.post('/login',function(req,res){
-  var userLogin = req.body;
-  //  Checking on empty entries
-  if(!userLogin.username || !userLogin.password){
-      console.log("Username/Password cannot be empty!");
-      res.render('login');
+        users.push(user2);  
+        fs.writeFileSync('users.json', JSON.stringify(users), 'utf-8', function(err) {
+          if (err) throw err
+        });
+        req.session.userId = user2.id;
+        res.redirect('/home')
+      }else{
+        res.redirect('/registration')
+      }
+    })
   }  
-  else{
-      //  Reading from the JSON DB
-      fs.readFile('users.json', 'utf-8', function(err, data) { 
 
+})
+
+app.post('/login', redirectHome, (req,res)=>{
+  const {username,password} = req.body
+  if (username&&password){
+    fs.readFile('users.json', 'utf-8', function(err, data) { 
         if (err) throw err;
         var existingUsers = JSON.parse(data);
-
-        //  Check if the username  exists
-        var nameFlag = false; 
-        for (let users of existingUsers.users) {
-          if(users.username == userLogin.username){
-            console.log('Username Found');  
-            nameFlag = true;
-            break;
-          }
-          else{
-            console.log('Username Not Found!');
-            res.render('login');
-            break;
-          }
+        var users = existingUsers
+        const user = users.find(
+          user => user.username === username && user.password === password
+        )  
+        if (user){
+          req.session.userId = user.id
+          res.redirect('/home')
+        } 
+        else{
+          res.redirect('/login')
         }
-        for (let users of existingUsers.users ){
-          if(nameFlag){
-            if(users.password == userLogin.password){
-              console.log('username & password are CORRECT');
-              res.render('home');
-              break;
-            }
-            else{
-              console.log('Password is NOT CORRECT');
-              res.render('login');
-              break;
-            }
-          }
-        }
-      });
-  }     
-});
+    }) 
+  } 
+})
 
-app.post('/search', function(req,res){
+app.post('/search', redirectLogin, (req,res) => {
   var userSearch =req.body.Search;
   fs.readFile('books.json', 'utf-8', function(err, data) { 
     if (err) throw err;
@@ -161,6 +203,16 @@ app.post('/search', function(req,res){
   
 });
 
-app.listen(port, function() {
-  console.log('Server running at http://localhost:' + port + '/');
+app.post('/logout', redirectLogin, (req,res) => {
+  req.session.destroy(err => {
+    if(err){
+      return res.redirect('/')
+    }
+
+    res.clearCookie(SESS_NAME)
+    return res.redirect('/login')
+  })
+});
+app.listen(PORT, function() {
+  console.log('Server running at http://localhost:' + PORT + '/');
 });
