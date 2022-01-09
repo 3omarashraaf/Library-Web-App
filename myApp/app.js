82c401ac12bd8667f1402b2b83a7232b9aed1701
Omar Awad <o.ashraaf@yahoo.com>
2022-01-09 21:09:25 +0100

    Adding documentation

--- myApp/app.js
+++ myApp/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('trust proxy', 1);
+app.set('trust proxy', 1); //Remove this line to run locally 
 app.use(session({
   name: SESS_NAME,
   resave: false,
@@ -29,10 +29,11 @@ app.use(session({
   cookie: {
       maxAge: SESS_LIFETIME,
       sameSite: true,
-      secure: true
+      secure: true //change 'true' to 'IN_PROD' to run locally
   }
 }))
 
+//Check if there is a logged in user to limit access to pages
 const redirectLogin = (req, res, next) =>{
   if (!req.session.userId){
       res.redirect('/')
@@ -40,6 +41,7 @@ const redirectLogin = (req, res, next) =>{
       next()
   }
 }
+//Check if there is a logged in user redirect to home page if yes
 const redirectHome = (req, res, next) =>{
   if (req.session.userId){
       res.redirect('/home')
@@ -47,12 +49,12 @@ const redirectHome = (req, res, next) =>{
       next()
   }
 }
+//Access user from res.locals 
 app.use((req, res, next) => {
   const{userId} = req.session
   fs.readFile('users.json', 'utf-8', function(err, data) { 
     if (err) throw err;
-    var existingUsers = JSON.parse(data);
-    var users = existingUsers
+    var users = JSON.parse(data);
     if (userId) {
       res.locals.user = users.find(
           user => user.id === userId
@@ -62,17 +64,18 @@ app.use((req, res, next) => {
   next()
 })
 
-
-
 // GET Requests 
 app.get('/', (req, res) => {
   const {userId} = req.session
+  //sending userId to frontend to show components based on if the user is logged in or not  
   res.render('start', {uid: userId})
 });
 app.get('/login', redirectHome,(req,res) => {
+  //sending empty error msg to frontend 
   res.render('login',{msg :''});
 });
 app.get('/registration', redirectHome,(req,res) => {
+    //sending empty error msg to frontend 
   res.render('registration', {msg: ''});
 });
 app.get('/home', redirectLogin,(req,res) => {
@@ -109,11 +112,11 @@ app.get('/readlist', redirectLogin, (req,res) => {
   const {userId} = req.session
   fs.readFile('users.json', 'utf-8', function(err, data) { 
     if (err) throw err;
-    var existingUsers = JSON.parse(data);
-    var users = existingUsers
+    var users = JSON.parse(data);
     const user = users.find(
       user => user.id === userId
     )
+    //sending user's saved books to the frontend to be displayed
     res.render('readlist',{title: `${user.books.length} Books found`,result: user.books, msg:''});
   })
 });
@@ -122,29 +125,34 @@ app.get('*', function(req, res){
 });
 // POST Requests 
 app.post('/register', redirectHome, (req,res)=>{
+  //getting username & password from request (user inputs)
   const {username,password} = req.body
   if (username&&password){
     fs.readFile('users.json', 'utf-8', function(err, data) { 
       if (err) throw err;
-      var existingUsers = JSON.parse(data);
-      var users = existingUsers;
+      var users = JSON.parse(data);
+      //Checking if username is already used
       const exists = users.some(
         user => user.username === username
       )
       if (!exists){
-        const user2 = {
+        //creating new user object
+        const newUser = {
           id: users.length + 1,
           username,
           password,
           books: []
         }
-        users.push(user2);  
+        //adding new user to JSON file
+        users.push(newUser);  
         fs.writeFileSync('users.json', JSON.stringify(users), 'utf-8', function(err) {
           if (err) throw err
         });
+        //Setting up session id to match the userId
         req.session.userId = user2.id;
         res.redirect('/home');
       }else{
+        //Sending error msg to the frontend if the entered username is already used  
         res.render('registration', {msg: 'Username is already used'});
 
       }
@@ -153,47 +161,88 @@ app.post('/register', redirectHome, (req,res)=>{
 
 })
 app.post('/login', redirectHome, (req,res)=>{
+  //getting username & password from request (user inputs)
   const {username,password} = req.body
   if (username&&password){
     fs.readFile('users.json', 'utf-8', function(err, data) { 
         if (err) throw err;
-        var existingUsers = JSON.parse(data);
-        var users = existingUsers
+        var users = JSON.parse(data);
+        //Checking if input credentials are in the JSON file
         const user = users.find(
           user => user.username === username && user.password === password
         )  
         if (user){
+          //Setting up session id to match the userId
           req.session.userId = user.id
           res.redirect('/home')
         } 
         else{
+          //Sending error msg to the frontend if the entered username or password is not matching the JSON file
           res.render('login', {msg : 'Incorrect Credential'});
         }
     }) 
   } 
 })
 app.post('/search', redirectLogin, (req,res) => {
+  //Getting user's search input from request
   var userSearch =req.body.Search;
   fs.readFile('books.json', 'utf-8', function(err, data) { 
     if (err) throw err;
     var existingBooks = JSON.parse(data);
     var resultBooks = [];
-    for (let book of existingBooks.books) {
-      let name =JSON.stringify(book.name);
+    for (var book of existingBooks.books) {
+      var name =JSON.stringify(book.name);
+      //checking if user's search input is a substring of any of the existing books 
       if (name.toLowerCase().includes(userSearch.toLowerCase())){
+        //adding the matched results into resultBooks
          resultBooks.push(book);
       }
     }  
-    if(resultBooks.length == 0){
-      res.render('searchresults',{title: `${resultBooks.length} Books found`,result: []});
-    }
-    else{
-
+      //If no books found --> '0 books found' + empty array
+      //if any books found --> 'No. books found' + resultBooks array
       res.render('searchresults',{title: `${resultBooks.length} Books found`,result: resultBooks});
-    }
   });     
-  
 });
+
+app.post('/addToList', redirectLogin, (req,res) => { 
+  // Getting active user's session id
+  var {userId} = req.session
+  //Getting the name of the choosed book
+  var name = req.body.Name;
+  fs.readFile('users.json', 'utf-8', function(err, data) { 
+    if (err) throw err;
+    var users = JSON.parse(data);
+    //Getting active user's data from JSON file
+    const user = users.find(
+      user => user.id === userId
+    )
+    //Checking if the choosed book is already in the user's list
+    const exists = user.books.some(
+      book => book.name === name
+    )
+    if (!exists){
+      fs.readFile('books.json', 'utf-8', function(err, data) { 
+        if (err) throw err;
+        var existingBooks = JSON.parse(data);
+        var books = existingBooks.books;
+        //Getting all the data related to the choosen book from Books.JSON
+        const book = books.find(
+          book => book.name === name
+        )
+        //Adding all the data related to the choosen book to the user's List
+        user.books.push(book); 
+        fs.writeFile('users.json', JSON.stringify(users), 'utf-8', function(err) {
+          if (err) throw err
+        });
+        res.redirect('/home')
+      });
+    }else{
+      //Sending error msg to frontend if book is already in user's list
+      res.render('readlist',{title: `${user.books.length} Books found`,result: user.books, msg:'This book already in your list'});
+    }
+    });
+  
+})
 app.post('/logout', redirectLogin, (req,res) => {
   req.session.destroy(err => {
     if(err){
@@ -203,40 +252,6 @@ app.post('/logout', redirectLogin, (req,res) => {
     res.redirect('/')
   })
 });
-app.post('/addToList', redirectLogin, (req,res) => { 
-  var {userId} = req.session
-  var name = req.body.Name;
-  fs.readFile('users.json', 'utf-8', function(err, data) { 
-    if (err) throw err;
-    var existingUsers = JSON.parse(data);
-    var users = existingUsers
-    const user = users.find(
-      user => user.id === userId
-    )
-    const exists = user.books.some(
-      book => book.name === name
-    )
-    if (!exists){
-      fs.readFile('books.json', 'utf-8', function(err, data) { 
-        if (err) throw err;
-        var existingBooks = JSON.parse(data);
-        var books = existingBooks.books;
-        const book = books.find(
-          book => book.name === name
-        )
-        user.books.push(book); 
-        fs.writeFile('users.json', JSON.stringify(users), 'utf-8', function(err) {
-          if (err) throw err
-        });
-        res.redirect('/home')
-      });
-    }else{
-      res.render('readlist',{title: `${user.books.length} Books found`,result: user.books, msg:'This book already in your list'});
-    }
-    });
-  
-})
- 
 if (process.env.PORT){
   app.listen(process.env.PORT, () => {
     console.log('Server running at Heroku');
