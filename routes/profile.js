6011b821beba31ced29f363c6477f9a9ed6c649c
Omar Awad <o.awad@gmx.com>
2022-08-02 15:35:30 +0200

    Deployment Attempt 9

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const profile = require('../controllers/profile');
+const public = require('../public');
+
 const { isLoggedIn , isNotLoggedIn,validateUserReg,validateUserLogin, allegedUser, duplicateKeys} = require('../utils/middleware');
 
 router.get('/', (req,res)=>{res.render('home')})                                   
