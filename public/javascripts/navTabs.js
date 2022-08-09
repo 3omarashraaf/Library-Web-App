e3f381bd9e3470f12dfd03020e97e97ba08b9dbd
Omar Awad <o.awad@gmx.com>
2022-08-09 18:03:59 +0200

    fix navBar focus

---
+++ public/javascripts/navTabs.js
@@ -0,0 +1,41 @@
+var navTabs = document.getElementsByClassName("nav-link")
+var activeTabs = document.getElementsByClassName("active")
+var homeActiveTabs = document.getElementsByClassName("text-white")
+
+var url = window.location.href
+const pieces = url.split(/[\s/]+/)
+var dir = pieces[2]
+switch (dir){
+    case "books": dir = "Books";
+                  break;
+    case "movies": dir = "Movies";
+                    break;  
+    case "tvshows": dir = "Tv Shows";
+                    break; 
+    case "login": dir = "Login"
+                  break;
+    case "register": dir = "Register"
+                  break;          
+    case "": dir = "Home";
+             break;
+    default: dir = "My Profile";
+             break;                       
+}
+window.onload = ()=> {
+    if(activeTabs[0]){
+        activeTabs[0].classList.remove("active")
+    }
+    if(homeActiveTabs[0]){
+        homeActiveTabs[0].classList.remove("text-white")
+    }
+    for (let navTab of navTabs){
+        if (navTab.textContent === dir){
+            if(dir === "Login" || dir === "Register" || dir === "Home"){
+                navTab.classList.add("text-white")
+            }else{
+                navTab.classList.add("active")
+            }
+        }
+    }
+}
+
